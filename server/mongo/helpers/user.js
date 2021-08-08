af278c8ef5e6031cf9a258a0a98f7576ca33a7e1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-08 18:43:32 +0530

    fixed some bugs

--- server/mongo/helpers/user.js
+++ server/mongo/helpers/user.js
@@ -1,39 +1,40 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User');
+const { generateToken } = require('../../utils/generator')
 
 module.exports = {
-  register: (userData) => {
+  register: ({ username, password }) => {
     return new Promise(async (resolve, reject) => {
-      const { username, password } = userData
-
       if (!username || !password) {
         reject('Required fields not found 🤷‍♂️')
         return
       }
 
       const userExists = await User.findOne({ username })
-
       if (userExists) {
         reject('User already exists 🤷‍♂️')
         return
       }
 
-      password = await bcrypt.hash(password, 10)
+      password = await bcrypt.hash(password, 12).catch(err => {
+        reject("Failed to hash")
+        return
+      });
 
-      const newUser = await User.create({ username, password }).catch(err => {
+      const userRes = await User.create({ username, password }).catch(err => {
         console.log(`[🙆‍♂️😭] failed to create user`)
         console.log("---\n", userData)
         reject('Failed to register 😓')
       })
 
       const token = generateToken({
-        ...newUser._doc,
-        id: newUser._id
+        ...userRes._doc,
+        id: userRes._id,
       });
 
       const data = {
-        username: newUser.username,
-        id: newUser._id,
+        username: userRes.username,
+        id: userRes._id,
         token
       }
 
