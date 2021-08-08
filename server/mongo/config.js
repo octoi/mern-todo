5dc92698a61156b5c398878830fe656c5b7c682f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-08 18:14:30 +0530

    config

--- server/mongo/config.js
+++ server/mongo/config.js
@@ -1,20 +1,18 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb://127.0.0.1:27017/merntodo'
+const { mongoDbUrl } = require('../utils/constants')
 
 const connectDb = async () => {
-  await mongoose.connect(CONNECTION_URL, {
+  await mongoose.connect(mongoDbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true,
   }).catch(err => {
-    console.log(`[😭] mongo db failed to connect to ${CONNECTION_URL}`)
+    console.log(`[😭] mongo db failed to connect to ${mongoDbUrl}`)
     console.log("---\n", err)
     process.exit(1)
   })
 
-  console.log(`[😀] mongo db connected to ${CONNECTION_URL}`)
+  console.log(`[😀] mongo db connected to ${mongoDbUrl}`)
 }
 
 module.exports = connectDb;
