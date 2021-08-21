c4cd72bf539cce267657504ab3bdf4fc4bf90409
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-21 14:58:47 +0530

    get todos route

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const todoHelper = require('../mongo/helpers/todo');
 const { generateErrorMessage, generateSuccessMessage } = require('../utils/generator')
 
+router.get('/', (req, res) => {
+  todoHelper.getAllTodos(req.body)
+    .then(data => res.json(generateSuccessMessage(data)))
+    .catch(data => res.json(generateErrorMessage(data)))
+});
+
 router.post('/create', (req, res) => {
   todoHelper.createTodo(req.body)
     .then(data => res.json(generateSuccessMessage(data)))
