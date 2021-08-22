d2a88a9e42084f4befcbfb7b0135c9f967a21a95
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-22 11:16:15 +0530

    login functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import Auth from '../components/Auth';
 import useAppContext from '../context/useAppContext';
-import { Flex, Input, Button, Heading, Link, useToast } from '@chakra-ui/react';
+import { Flex, Input, Button, Heading, Link, useToast, Spinner } from '@chakra-ui/react';
 import { login as requestLogin } from '../api/user';
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const showToast = useToast();
   const { setUser } = useAppContext();
 
 
-  const login = async () => {
-    const userData = await requestLogin(username, password).catch(err => {
+  const login = () => {
+    setLoading(true)
+    requestLogin(username, password).then((userData) => {
+      setLoading(false)
+      setUser(userData);
+    }).catch(err => {
+      setLoading(false)
       showToast({
         title: err ? err : 'Failed to login :(',
         position: 'top-right',
@@ -23,7 +29,6 @@ export default function Login() {
       })
     });
 
-    console.log(userData)
   }
 
   return (
@@ -51,8 +56,8 @@ export default function Login() {
             colorScheme="teal"
             width="100%"
             onClick={login}
-            disabled={username.trim().length === 0 || password.length === 0}
-          >Log In</Button>
+            disabled={username.trim().length === 0 || password.length === 0 || loading}
+          >{loading ? <Spinner /> : 'Log In'}</Button>
           <Link mt={2} href='/register'>New to mern todo ?? Register</Link>
         </Flex>
       </Flex>
