4ac77daa737820b5d67943e08a99c0d1c02c8f92
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-22 10:27:58 +0530

    routes & pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Chakra from './components/Chakra';
 import { Context } from './context/Context';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
   return (
     <Chakra>
       <Context>
-
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </Router>
       </Context>
     </Chakra>
   )
